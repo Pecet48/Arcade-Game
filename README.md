9c25856be8d1da338cc7fcf21b29fc007ecbd822
Pecet48 <pecet63@gmail.com>
2018-08-15 20:01:20 +0200

    The final commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Simple Arcade Game
+
+Simple arcade game from udacity nanodegree program.
+
+You must avoid colliding with worms and reach the river!
+
+### Installing
+
+If you want play, you just need to download files.
+
+You can clone them
+```
+git clone https://github.com/Pecet48/Arcade-Game.git
+```
+
+or just click download ZIP above and... unzip
+
+## Built
+
+HTML, CSS, JS with start code from udacity.
---
+++ js/app.js
@@ -0,0 +1,116 @@
+// Enemies our player must avoid
+var Enemy = function(x,y,speed) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.sprite = 'images/enemy-bug.png';
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+
+    this.x += this.speed * dt;
+
+    if(this.x > 500) {
+        this.x = -100;
+    }
+
+    if(player.x > this.x - 60 && player.x < this.x + 60 && player.y < this.y + 25 && player.y > this.y - 25) {
+        player.x = 200;
+        player.y = 375;
+    }
+
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+
+var Player = function(x,y){
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/char-boy.png';
+}
+
+Player.prototype.update = function() {
+    
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(way) {
+    if(way == 'left') {
+        this.x = this.x - 100;
+        if(this.x < 0) {
+            this.x = this.x + 100;
+            console.log('KONIEC MAPY!');
+        }
+    } else if(way == 'up') {
+        this.y = this.y - 82;
+        if(this.y < -100) {
+            this.y = this.y + 82;
+            console.log('KONIEC MAPY!');
+        }
+        if(this.y < 0) {
+            player.x = 200;
+            player.y = 375;
+            alert('Congratulations! You win :)');
+        }
+    } else if(way == 'right') {
+        this.x = this.x + 100;
+        if(this.x > 400) {
+            this.x = this.x - 100;
+            console.log('KONIEC MAPY!');
+        }
+    } else if(way == 'down') {
+        this.y = this.y + 82;
+        if(this.y > 400) {
+            this.y = this.y - 82;
+            console.log('KONIEC MAPY!');
+        }
+    }
+
+};
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+
+var allEnemies = [];
+
+var player = new Player(200,375);
+var enemy = new Enemy(-100,215,100);
+var enemy2 = new Enemy(-100,135,300);
+var enemy3 = new Enemy(-100,60,150);
+
+allEnemies.push(enemy,enemy2,enemy3);
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
